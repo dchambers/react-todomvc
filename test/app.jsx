9c1d20cd6bf54acc8741572529a8dd55fedc3df2
dchambers <dominic.chambers@gmail.com>
2015-10-12 13:43:18 +0100

    Renamed 'js' directory to 'src'
    This is the convention I normally use, and keeping things standard makes
    it easier to share configuration between projects.

--- test/app.jsx
+++ test/app.jsx
@@ -6,9 +6,9 @@ var expect = unexpected.clone().installPlugin(unexpectedReactShallow);
 var React = require('react');
 var ReactTestUtils = require('react-addons-test-utils');
 var renderer = ReactTestUtils.createRenderer();
-var TodoApp = require('../js/TodoApp.jsx');
-var TodoHeader = require('../js/TodoHeader.jsx');
-var TodoModel = require('../js/TodoModel.js');
+var TodoApp = require('../src/TodoApp.jsx');
+var TodoHeader = require('../src/TodoHeader.jsx');
+var TodoModel = require('../src/TodoModel.js');
 
 describe('TodoMVC App', function() {
   it('only renders a header when there are no items in the list', function() {
