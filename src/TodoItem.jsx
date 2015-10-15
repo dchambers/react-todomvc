dchambers/react-todomvc

commit 3558cc4a62df6ba796279988f297674b528eb0aa

Make TodoItem more testable
Make `title` and `completed` properties so the TodoItem component is
easier to test.

src/TodoItem.jsx
@@ -18,12 +18,12 @@ var TodoItem = React.createClass({
 
 	handleEdit: function () {
 		this.props.onEdit();
-		this.setState({editText: this.props.todo.title});
+		this.setState({editText: this.props.title});
 	},
 
 	handleKeyDown: function (event) {
 		if (event.which === ESCAPE_KEY) {
-			this.setState({editText: this.props.todo.title});
+			this.setState({editText: this.props.title});
 			this.props.onCancel(event);
 		} else if (event.which === ENTER_KEY) {
 			this.handleSubmit(event);
@@ -35,7 +35,7 @@ var TodoItem = React.createClass({
 	},
 
 	getInitialState: function () {
-		return {editText: this.props.todo.title};
+		return {editText: this.props.title};
 	},
 
 	/**
@@ -70,18 +70,18 @@ var TodoItem = React.createClass({
 	render: function () {
 		return (
 			<li className={classNames({
-				completed: this.props.todo.completed,
+				completed: this.props.completed,
 				editing: this.props.editing
 			})}>
 				<div className="view">
 					<input
 						className="toggle"
 						type="checkbox"
-						checked={this.props.todo.completed}
+						checked={this.props.completed}
 						onChange={this.props.onToggle}
 					/>
 					<label onDoubleClick={this.handleEdit}>
-						{this.props.todo.title}
+						{this.props.title}
 					</label>
 					<button className="destroy" onClick={this.props.onDestroy} />
 				</div>

src/TodoApp.jsx
@@ -88,7 +88,8 @@ var TodoApp = React.createClass({
 			return (
 				<TodoItem
 					key={todo.id}
-					todo={todo}
+					title={todo.title}
+					completed={todo.completed}
 					onToggle={this.toggle.bind(this, todo)}
 					onDestroy={this.destroy.bind(this, todo)}
 					onEdit={this.edit.bind(this, todo)}

test/app.jsx
@@ -20,7 +20,7 @@ describe('TodoMVC App', function() {
   it('only renders a header when there are no items in the list', function() {
     // given
     renderer.render(<TodoApp model={new TodoModel()}/>);
-    
+
     // then
     expect(renderer, 'to have rendered',
       <Container componentName="TodoApp">
@@ -29,14 +29,13 @@ describe('TodoMVC App', function() {
     );
   });
 
-  // TODO: test the tile and completed status, e.g. `todo={{title: 'A', completed:false}}`
   it('allows an item to be added to the list', function() {
     // given
     var todoApp = <TodoApp model={new TodoModel()}/>;
     var inputBox = $(todoApp).find('input.new-todo').dom();
 
     // when
-    inputBox.value = 'A';
+    inputBox.value = 'Stuff';
     ReactTestUtils.Simulate.keyDown(inputBox, {key: 'Enter', keyCode: 13, which: 13});
 
     // then
@@ -45,7 +44,7 @@ describe('TodoMVC App', function() {
       <Container componentName="TodoApp">
         <TodoHeader/>
         <TodoItems activeTodoCount={1}>
-          <TodoItem/>
+          <TodoItem title="Stuff" completed={false}/>
         </TodoItems>
         <TodoFooter count={1} completedCount={0} nowShowing="all"/>
       </Container>

commit 54f284cba05fd1972bf31601035b59d3789f5314

Fix rendering bug
This fixes a bug that was introduced in 3558cc4 (Make TodoItem more
testable).

src/TodoItem.jsx
@@ -47,7 +47,8 @@ var TodoItem = React.createClass({
 	 */
 	shouldComponentUpdate: function (nextProps, nextState) {
 		return (
-			nextProps.todo !== this.props.todo ||
+			nextProps.title !== this.props.title ||
+			nextProps.completed !== this.props.completed ||
 			nextProps.editing !== this.props.editing ||
 			nextState.editText !== this.state.editText
 		);